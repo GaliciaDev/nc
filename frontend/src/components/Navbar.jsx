4c418d8980619f23c3594b64db5da06b1eeb7eaa
GaliciaDev <mydevgalicia@gmail.com>
2024-07-26 12:14:05 -0600

    Pruebas de depuracion

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const Navbar = () => {
+  return (
+    <section>
+        <p>Verificando rutas.....</p>
+    </section>
+  );
+};
+
+export default Navbar;
+
+
+
+
+
